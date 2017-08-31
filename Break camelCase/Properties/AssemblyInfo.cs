3141451523767aa3c9634f4a08bb949dede9ec51
dlgs5100 <dlgs5100@gmail.com>
2017-08-31 11:20:12 +0800

    first commit

---
+++ Break camelCase/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Break camelCase")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Break camelCase")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("80b7cbce-6ab2-4c67-87e5-c08096043e5c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Break camelCase/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Break_camelCase
+{
+    [TestFixture]
+    public class Sample_Tests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData("camelCasing").Returns("camel Casing");
+                yield return new TestCaseData("camelCasingTest").Returns("camel Casing Test");
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public string Test(string str) => Kata.BreakCamelCase(str);
+    }
+
+    public class Kata
+    {
+        public static string BreakCamelCase(string str)
+        {
+            int cut = 0;
+            string result="";
+
+            foreach(var item in str.Select((v, i) => new { Value = v, Index = i }))
+            {
+                if (Char.IsUpper(item.Value) && item.Index + 1 == str.Length)
+                {
+                    result += str.Substring(cut, item.Index - cut) + " ";
+                    result += item.Value.ToString();
+                }
+                else if (Char.IsUpper(item.Value))
+                {
+                    result += str.Substring(cut, item.Index - cut) + " ";
+                    cut = item.Index;
+                }
+                else if (item.Index + 1 == str.Length)
+                    result += str.Substring(cut, item.Index - cut + 1);
+                
+            }
+            return result;
+        }
+    }
+}
